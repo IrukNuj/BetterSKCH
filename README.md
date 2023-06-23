756592813cd1b617d232f02e80b06cdb046cf538
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2023-06-23 06:19:07 +0000

    ✨ とりあえず野郎を消す

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # ChromeExtensionTemplateForDeno
 
 # Required development tools
+
 - deno : `v1.29.1` or higher
 
-Haven't installed Deno yet?  
-Let's install Deno now! ↓↓↓↓  
+Haven't installed Deno yet?
+Let's install Deno now! ↓↓↓↓
 https://deno.land/
 
 # Manifest
+
 - `public\manifest.json` manifest version 3
 
 # Build
@@ -15,18 +17,21 @@ https://deno.land/
 ```
 deno task build
 ```
+
 Build output `/dist`
 
 caution :
-Use the `deno bundle` command to build. As of `v1.29.1` the `deno bundle` does not support NPM yet, so do not reference NPM in your code.  
+Use the `deno bundle` command to build. As of `v1.29.1` the `deno bundle` does not support NPM yet, so do not reference NPM in your code.
 If you really want to use the npm library now, you may be able to use it by referring to https://unpkg.com/ as a workaround.
 
 # VSCode settings
+
 - Enable deno
 - Enable lint
 - Enable format on save
 
 # Type safety
+
 The following types used in extensions have type safety enabled.
 
 - `chrome`
--- src/content.ts
+++ src/content.ts
@@ -4,32 +4,39 @@
 //   return true;
 // });
 
-const AD_SCRIPT_SRC =
-  'https://adm.shinobi.jp/s/2aa1bd746a9c3f5d6208776028f3c934';
+// const AD_SCRIPT_SRC =
+//   'https://adm.shinobi.jp/s/2aa1bd746a9c3f5d6208776028f3c934';
 
 const ONE_LINE_COUNT = 3;
-
-console.log('hello~!');
 
 const removeElements = (elements: NodeListOf<HTMLElement>): void => {
   elements.forEach((e) => e.remove());
 };
 
+// const insertAds = () => {
+//   const posts = document.getElementById('posts');
+//   const newAd = document.createElement('script');
+
+//   newAd.src = AD_SCRIPT_SRC;
+//   newAd.type = 'text/javascript';
+//   newAd.async = true;
+//   posts?.appendChild<HTMLElement>(newAd);
+// };
+
 const main = (): void => {
   const malePosts = document.querySelectorAll<HTMLElement>('.gender1'); //https://developer.hatenastaff.com/entry/2020/12/12/121212
-  removeElements(malePosts);
-
-  const ads = document.querySelectorAll<HTMLElement>('.ad');
-  removeElements(ads);
-
-  const autopagerizePageSeparator = document.querySelectorAll<HTMLElement>(
-    '.autopagerize_page_separator, .autopagerize_link, .autopagerize_page_info',
-  );
-  removeElements(autopagerizePageSeparator);
-
   const femalePosts = document.querySelectorAll<HTMLElement>(
     '.gender2, .gender3',
   );
+  const autopagerizePageSeparator = document.querySelectorAll<HTMLElement>(
+    '.autopagerize_page_separator, .autopagerize_link, .autopagerize_page_info',
+  );
+  const ads = document.querySelectorAll<HTMLElement>('.ad');
+
+  removeElements(malePosts);
+  removeElements(ads);
+  removeElements(autopagerizePageSeparator);
+
   femalePosts.forEach((e, count) => {
     e.classList.remove('first', 'end');
 
@@ -42,15 +49,6 @@ const main = (): void => {
         break;
     }
   });
-  console.log(femalePosts);
-
-  const posts = document.getElementById('posts');
-  const newAd = document.createElement('script');
-
-  newAd.src = AD_SCRIPT_SRC;
-  newAd.type = 'text/javascript';
-  newAd.async = true;
-  posts?.appendChild<HTMLElement>(newAd);
 };
 
 self.addEventListener('load', main);
