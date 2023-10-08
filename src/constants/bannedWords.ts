074728300a2ec75a3e2c90a331bbca924e3ec2af
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2023-10-08 14:44:43 +0000

    update

--- src/constants/bannedWords.ts
+++ src/constants/bannedWords.ts
@@ -1,8 +1,8 @@
 import { BannedWords } from '../type/banWords.ts';
 
-const isDev = false;
+const isDev = true;
 const adminBannedWords: BannedWords = [
-  '依存',
+  // '依存',
   'valorant',
   'FF14',
   'LOL',
